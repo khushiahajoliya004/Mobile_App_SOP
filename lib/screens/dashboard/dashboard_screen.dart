717041e36652cc18d73002a335150939525cdac2
priyanshulumoslogic-ux <priyanshu.lumoslogic@gmail.com>
2026-06-10 16:58:49 +0530

    fix 2

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -50,9 +50,7 @@ class _DashboardScreenState extends State<DashboardScreen>
     setState(() => _loading = true);
     _user = await _auth.getUser();
     try {
-      final res = _user?.isCompanyAdmin == true || _user?.isSuperAdmin == true
-          ? await _api.getCompanyDashboard(period: '30d')
-          : await _api.getMyDashboard(period: '30d');
+      final res = await _api.getCompanyDashboard(period: '30d');
       final raw = res.data;
       // Handle: raw, raw['data'], or raw['data']['stats'] nesting
       final dataMap = raw is Map
