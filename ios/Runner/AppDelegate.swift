1759ad1df08e6d87e8f165e55a42ac7c5008814e
Jignesh Patel <p.jignesh23@gmail.com>
2026-06-11 17:17:14 +0530

    Implemented push notification from IOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import Firebase
+import UserNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,15 +9,32 @@ import UIKit
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        FirebaseApp.configure()
+
+        UNUserNotificationCenter.current().delegate = self
+
         GeneratedPluginRegistrant.register(with: self)
-        
+
         // Register native audio recorder plugin
         if let controller = window?.rootViewController as? FlutterViewController {
             AudioRecorderPlugin.register(
                 with: controller.registrar(forPlugin: "AudioRecorderPlugin")!
             )
         }
-        
+
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    // Show notifications when app is in foreground
+    override func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        if #available(iOS 14.0, *) {
+            completionHandler([.banner, .badge, .sound])
+        } else {
+            completionHandler([.alert, .badge, .sound])
+        }
     }
 }
